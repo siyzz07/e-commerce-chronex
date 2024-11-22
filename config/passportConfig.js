b95b472a4043d382add7ddfd0890fc906afde900
shibin <shibin>
2024-11-22 11:43:03 +0530

    rout changet (aftere hosting)

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -7,7 +7,7 @@ const User = require('../models/userModels');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "http://chronex.siyzz.site/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
